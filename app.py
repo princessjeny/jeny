c86b8fa827fa36d7b1c74710c000cb446a6bec79
princessjeny <2jenyyyyy@gmail.com>
2024-10-21 10:43:29 +0700

    Week 4

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.roiwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI") 
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[str(DB_NAME)]
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -27,3 +37,9 @@ def homework_get():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+
+# Nama: Jeny febri lestari
+# Kelas: 11 TKJ 3
+# Absen: 09
